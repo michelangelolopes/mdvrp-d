9f9f87d8aab9a1d544fe9cff8fff8e00aee5fb8c
Michelangelo Lopes <michelangelopes@yahoo.com.br>
2024-10-13 22:26:42 -0300

    Adicionando classes relacionadas com a solução Route e Solution

---
+++ include/solution/Route.h
@@ -0,0 +1,9 @@
+#ifndef ROUTE_H
+#define ROUTE_H
+
+class Route {
+    public:
+        int* visitedCustomers;
+};
+
+#endif
---
+++ include/solution/Solution.h
@@ -0,0 +1,11 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include "Route.h"
+
+class Solution {
+    public:
+        Route* bestRoutes;
+};
+
+#endif
