f63f43fb7d6cb64b416b1428482316be52f8ac7f
Michelangelo Lopes <michelangelopes@yahoo.com.br>
2024-10-20 13:05:05 -0300

    Melhorando Makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,22 @@
+INCLUDE=include/clustering/* include/metaheuristics/* include/problem/* include/solution/* include/utils/*
+SRC=src/clustering/* src/metaheuristics/* src/problem/* src/utils/* src/solution/*
+
 compile:
 	@echo "compiling..."
-	@g++ -o ./bin/mdvrp-d include/clustering/* include/metaheuristics/* include/problem/*  include/utils/* src/clustering/* src/metaheuristics/* src/problem/* src/utils/* src/main.cpp
+	@g++ -o ./bin/mdvrp-d $(INCLUDE) $(SRC) src/main.cpp
 
 compile_debug:
 	@echo "compiling in debug mode..."
-	@g++ -g -DDEBUG -o ./bin/mdvrp-d include/clustering/* include/metaheuristics/* include/problem/*  include/utils/* src/clustering/* src/metaheuristics/* src/problem/* src/utils/* src/main.cpp
+	@g++ -g -DDEBUG -o ./bin/mdvrp-d $(INCLUDE) $(SRC) src/main.cpp
 
 run:
 	@echo "running..."
-	@./bin/mdvrp-d p01-D.mdvrp-d
+	@./bin/mdvrp-d
+
+run_gdb:
+	@echo "running with gdb..."
+	@gdb -ex=r --args ./bin/mdvrp-d
 
 run_valgrind:
 	@echo "running with valgrind..."
-	@valgrind -s --leak-check=full --track-origins=yes ./bin/mdvrp-d p01-D.mdvrp-d
+	@valgrind -s --leak-check=full --show-leak-kinds=all --track-origins=yes ./bin/mdvrp-d
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 
@@ -8,24 +9,23 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    string datasetFilePath = "../Dataset/2_MDVRP-D/MDVRP-D-Benchmarks/";
-    if(argc >= 2) {
-        datasetFilePath += argv[1];
-        std::cout << datasetFilePath << '\n';
-    } else {
-        datasetFilePath += "p01-D.mdvrp-d";
-    }
+    string datasetFilePath = "../Dataset/2_MDVRP-D/MDVRP-D-Benchmarks/p01-D.mdvrp-d";
+    // if(argc >= 2) {
+    //     datasetFilePath += argv[1];
+    //     std::cout << datasetFilePath << '\n';
+    // }
+
 
     ProblemInstance problemInstance(datasetFilePath);
 
-    int antsCount = 5; //192;
+    int antsCount = 192; //192;
     double pheromoneUpdateCoef = 3; 
     double temperatureUpdateCoef = 0.1;
     double temperatureCoolingCoef = 1;
-    int sectorsCount = 5; //16;
-    int subClusterSize = 5;//24;
+    int sectorsCount = 16; //16;
+    int subClusterMaxSize = 24;//24;
     int localOptimizationFrequency = 10;
-    int primaryClustersCount = 4;
+    int primarySubClustersMaxCount = 4;
     double pheromoneEvaporationCoefMin = 0.001;
     double pheromoneEvaporationCoefMax = 0.1;
     double pheromoneProbabilityCoef = 1;
@@ -37,8 +37,8 @@ int main(int argc, char** argv) {
             temperatureUpdateCoef,
             temperatureCoolingCoef,
             sectorsCount, 
-            primaryClustersCount,
-            subClusterSize,
+            primarySubClustersMaxCount,
+            subClusterMaxSize,
             localOptimizationFrequency,
             pheromoneEvaporationCoefMin,
             pheromoneEvaporationCoefMax,
@@ -46,16 +46,37 @@ int main(int argc, char** argv) {
             pheromoneProbabilityCoef
     );
 
-    #ifdef DEBUG
-        int printDistanceMatrix = 0;
-        problemInstance.print(printDistanceMatrix);
-        antSystem.customerClusters[0].print();
-        antSystem.customerClusters[problemInstance.customerCount].print();
-        antSystem.customerClusters[problemInstance.customerCount + 1].print();
-        antSystem.frame.print();
-    #endif
+    // #ifdef DEBUG
+    //     int printDistanceMatrix = 0;
+    //     problemInstance.print(printDistanceMatrix);
+    // antSystem.customerClusters[0].print();
+    // std::cout << "\n";
+    
+    // antSystem.depotClusters[0].print();
+    // std::cout << "\n";
+    //     antSystem.frame.print();
+    // #endif
+
+    // Solution test(1, 7);
+    // test.routes[0].visitedVertices[0] = 50;
+    // test.routes[0].visitedVertices[1] = 45;
+    // test.routes[0].visitedVertices[2] = 38;
+    // test.routes[0].visitedVertices[3] = 18;
+    // test.routes[0].visitedVertices[4] = 19;
+    // test.routes[0].visitedVertices[5] = 5;
+    // test.routes[0].visitedVertices[6] = 50;
+    // test.routes[0].routeRealLength = 7;
+    // test.calculateFitness(problemInstance);
+    // test.print();
+    // test.finalize();
+
+    auto start = std::chrono::high_resolution_clock::now();
 
     antSystem.run();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Execution time: " << duration.count() << " seconds\n";
 
     antSystem.finalize();
 
