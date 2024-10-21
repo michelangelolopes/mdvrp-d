2cec824cf1e0865857c819d5834b23247f531355
Michelangelo Lopes <michelangelopes@yahoo.com.br>
2024-10-21 12:10:51 -0300

    Adicionando novos template methods para ArrayUtils

--- include/utils/ArrayUtils.h
+++ include/utils/ArrayUtils.h
@@ -2,15 +2,39 @@
 #define ARRAY_UTILS_H
 
 #include <cstdlib>
+#include <iostream>
 
 void** callocMatrix(int count, int pointerSize, int objectSize);
 void** callocMatrix(int pointerCount, int objectCount, int pointerSize, int objectSize);
+
+template <typename T> void printIndexesArray(T* array, int count, T value) {
+    
+    for(int i = 0; i < count; i++) {
+        
+        if(array[i] == value) {
+            std::cout << i << " ";
+        }
+    }
+
+    std::cout << "\n";
+}
 
 template <typename T> T sumArray(T* array, int count) {
     
     T sum = 0;
     for(int i = 0; i < count; i++) {
         sum += array[i];
+    }
+
+    // return std::accumulate(array, array + count, 0);
+    return sum;
+}
+
+template <typename T> T sumMatrix(T** matrix, int count) {
+    
+    T sum = 0;
+    for(int i = 0; i < count; i++) {
+        sum += sumArray(matrix[i], count);
     }
 
     // return std::accumulate(array, array + count, 0);
