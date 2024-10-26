31d4209c8251978421ee57f7f33ac2c640c8aaa6
Michelangelo Lopes <michelangelopes@yahoo.com.br>
2024-10-26 16:07:51 -0300

    Definindo método de MathUtils como inline

--- include/utils/MathUtils.hpp
+++ include/utils/MathUtils.hpp
@@ -1,8 +1,12 @@
 #ifndef MATH_UTILS_HPP
 #define MATH_UTILS_HPP
 
+#include <cmath>
 #include "../general/Position2D.hpp"
 
-double calculateEuclidianDistance(Position2D positionA, Position2D positionB);
+inline double calculateEuclidianDistance(Position2D positionA, Position2D positionB) {
+
+    return sqrt(pow(positionB.x - positionA.x, 2) + pow(positionB.y - positionA.y, 2));
+}
 
 #endif
--- src/utils/MathUtils.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../include/utils/MathUtils.hpp"
-
-#include <cmath>
-
-double calculateEuclidianDistance(Position2D positionA, Position2D positionB) {
-
-    return sqrt(pow(positionB.x - positionA.x, 2) + pow(positionB.y - positionA.y, 2));
-}
